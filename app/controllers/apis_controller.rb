39183b2818cad1fc26d72220573cb6817dae1fc5
Shin'ya Ueoka <ibenza@i-beam.org>
2015-09-11 23:25:18 +0900

    Change upload place to storage in server

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,5 +1,3 @@
-require 'digest/md5'
-
 class ApisController < ApplicationController
   # TODO: not to use
   include ApplicationHelper
@@ -17,34 +15,35 @@ class ApisController < ApplicationController
   #     ]
   #  }
   def file_receiver
-    s3_uploader = S3Uploader.new(bucket: "#{Settings.s3.bucket_name}/1/#{current_user.id}")
-
-    s3_files = []
+    uploads = []
 
     params[:files].each do |file|
       # Skip uploading if file ext is not listed.
       next unless file.original_filename =~ /\.(jpe?g|png|gif|pdf)\Z/
 
-      object_file_name = "#{Digest::MD5.file(file.path)}#{File.extname(file.original_filename)}"
-      res = s3_uploader.upload!(file: file.path, name: object_file_name)
+      hash_value = Digest::MD5.file(file.path)
+      object_file_name = "#{hash_value}#{File.extname(file.original_filename)}"
+      uploaded_file = UploadedFile.new(file: file.path, name: object_file_name)
+      uploaded_file.save
 
       case file.original_filename
       when /\.(jpe?g|png|gif)\Z/
-        s3_files << { type: 'image', name: file.original_filename, image: res.public_url.to_s }
+        uploads << { type: 'image', name: file.original_filename, image: uploaded_file.url }
       when /\.pdf\Z/
         if Settings.enable_pdf_uploading
-          cover_image_name = "#{Digest::MD5.file(file.path)}-cover.png"
+          cover_image_name = "#{hash_value}-cover.png"
           pdf = Magick::ImageList.new(file.path + '[0]')
           cover_tmp = Rails.root.join('tmp', cover_image_name)
           pdf[0].write(cover_tmp)
-          cover_res = s3_uploader.upload!(file: cover_tmp, name: cover_image_name)
-
-          s3_files << { type: 'slide', name: cover_image_name, url: res.public_url.to_s, image: cover_res.public_url.to_s }
+          cover_file = UploadedFile.new(file: cover_tmp, name: cover_image_name)
+          cover_file.save
+          uploads << { type: 'slide', name: cover_image_name, url: uploaded_file.url,
+                       image: cover_file.url }
         end
       end
     end
 
-    render json: { status: 'OK', files: s3_files, uploading_index: params[:uploading_index] }
+    render json: { status: 'OK', files: uploads, uploading_index: params[:uploading_index] }
   end
 
   def user_mention
